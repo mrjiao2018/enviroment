756175471f4bd1eb7ccd33c26a0fd432c8290aa8
lzb6666 <31194937+lzb6666@users.noreply.github.com>
2018-07-12 14:11:03 +0800

    url

--- src/main/java/enviroment/controller/CalcController.java
+++ src/main/java/enviroment/controller/CalcController.java
@@ -21,6 +21,130 @@ public class CalcController {
         return gson.toJson(a1);
     }
 
+    @ResponseBody
+    @RequestMapping("/B1")
+    public String calcB1(A1 a1){
+        a1.cale();
+        return gson.toJson(a1);
+    }
+
+    @ResponseBody
+    @RequestMapping("/B2")
+    public String calcB2(A1 a1){
+        a1.cale();
+        return gson.toJson(a1);
+    }
+
+    @ResponseBody
+    @RequestMapping("/C1")
+    public String calcC1(A1 a1){
+        a1.cale();
+        return gson.toJson(a1);
+    }
+
+    @ResponseBody
+    @RequestMapping("/D1")
+    public String calcD1(A1 a1){
+        a1.cale();
+        return gson.toJson(a1);
+    }
 
 
+    @ResponseBody
+    @RequestMapping("/D2")
+    public String calcD2(A1 a1){
+        a1.cale();
+        return gson.toJson(a1);
+    }
+
+    @ResponseBody
+    @RequestMapping("/E1")
+    public String calcE1(A1 a1){
+        a1.cale();
+        return gson.toJson(a1);
+    }
+
+    @ResponseBody
+    @RequestMapping("/B2")
+    public String calcE21(A1 a1){
+        a1.cale();
+        return gson.toJson(a1);
+    }
+
+    @ResponseBody
+    @RequestMapping("/B2")
+    public String calcE22(A1 a1){
+        a1.cale();
+        return gson.toJson(a1);
+    }
+
+    @ResponseBody
+    @RequestMapping("/F1")
+    public String calcF1(A1 a1){
+        a1.cale();
+        return gson.toJson(a1);
+    }
+
+    @ResponseBody
+    @RequestMapping("/F2")
+    public String calcF2(A1 a1){
+        a1.cale();
+        return gson.toJson(a1);
+    }
+
+    @ResponseBody
+    @RequestMapping("/G1")
+    public String calcG1(A1 a1){
+        a1.cale();
+        return gson.toJson(a1);
+    }
+
+    @ResponseBody
+    @RequestMapping("/H1")
+    public String calcH1(A1 a1){
+        a1.cale();
+        return gson.toJson(a1);
+    }
+
+    @ResponseBody
+    @RequestMapping("/H2")
+    public String calcH2(A1 a1){
+        a1.cale();
+        return gson.toJson(a1);
+    }
+
+    @ResponseBody
+    @RequestMapping("/H3")
+    public String calcH3(A1 a1){
+        a1.cale();
+        return gson.toJson(a1);
+    }
+
+    @ResponseBody
+    @RequestMapping("/H4")
+    public String calcH4(A1 a1){
+        a1.cale();
+        return gson.toJson(a1);
+    }
+
+    @ResponseBody
+    @RequestMapping("/I1")
+    public String calcI1(A1 a1){
+        a1.cale();
+        return gson.toJson(a1);
+    }
+
+    @ResponseBody
+    @RequestMapping("/J1")
+    public String calcJ1(A1 a1){
+        a1.cale();
+        return gson.toJson(a1);
+    }
+
+    @ResponseBody
+    @RequestMapping("/K1")
+    public String calcK1(A1 a1){
+        a1.cale();
+        return gson.toJson(a1);
+    }
 }
