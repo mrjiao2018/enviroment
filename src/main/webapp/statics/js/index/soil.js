0c1d8687b79797910536d863999468491f8fa299
lzb6666 <echol@qq.com>
2018-10-05 18:10:43 +0800

    单位调整

--- src/main/webapp/statics/js/index/soil.js
+++ src/main/webapp/statics/js/index/soil.js
@@ -15,7 +15,7 @@ formArray[1]={
     title:"征地区片价修正法土地经济价值",
     input_lable:["征地区片价", "开垦费标准", "新增费标准","社保费标准","耕作层剥离费标准","土地面积"],
     output_lable:["土地经济价值"],
-    input_lable_unit:["元.m<sup>3</sup>", "", "m<sup>2</sup>"],
+    input_lable_unit:["元/m<sup>2</sup>", "m<sup>2</sup>", "m<sup>2</sup>","m<sup>2</sup>","m<sup>2</sup>","m<sup>2</sup>"],
     output_lable_unit:["元"],
     input_name:[],
     output_name:[]
@@ -26,7 +26,7 @@ formArray[2]={
     title:"成本法土地经济价值",
     input_lable:["土地取得费", "土地开发费", "利息","利润","土地增值收益","土地面积"],
     output_lable:["土地经济价值"],
-    input_lable_unit:["元.m<sup>3</sup>", "", "m<sup>2</sup>"],
+    input_lable_unit:["元/m<sup>2</sup>", "元/m<sup>2</sup>","","", "元/m<sup>2</sup>","m<sup>2</sup>"],
     output_lable_unit:["元"],
     input_name:[],
     output_name:[]
@@ -37,7 +37,7 @@ formArray[2]={
     title:"土地生态服务价值计算模型",
     input_lable:["土地利用类型分布面积", "单位面积生态价值系数"],
     output_lable:["土地生态系统服务总价值"],
-    input_lable_unit:["元.m<sup>3</sup>", "", "m<sup>2</sup>"],
+    input_lable_unit:["m<sup>2</sup>", ""],
     output_lable_unit:["元"],
     input_name:[],
     output_name:[]
@@ -47,7 +47,7 @@ formArray[3]={
     title:"社会保障价值",
     input_lable:["城乡平均收入","常住人口数"],
     output_lable:["土地资源提供的社会保障价值"],
-    input_lable_unit:["元.m<sup>3</sup>", "", "m<sup>2</sup>"],
+    input_lable_unit:["元", ""],
     output_lable_unit:["元"],
     input_name:[],
     output_name:[]
